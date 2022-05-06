6378bd5ed6976a7e5ad9322cf4a13c0141ed78fc
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-05-06 10:12:05 +0200

    add init function for user table

--- news-app/libs/api-model/src/lib/user/model.user.ts
+++ news-app/libs/api-model/src/lib/user/model.user.ts
@@ -3,6 +3,7 @@ import {
   InferAttributes,
   InferCreationAttributes,
   CreationOptional,
+  DataTypes,
 } from 'sequelize';
 
 /**
@@ -30,6 +31,28 @@ export class User
   declare id: CreationOptional<number>;
   declare email: string;
   declare password: string;
+}
+
+export function initUserTableSQL() {
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+    },
+    { sequelize: null as any, tableName: 'users' }
+  );
 }
 
 /**
