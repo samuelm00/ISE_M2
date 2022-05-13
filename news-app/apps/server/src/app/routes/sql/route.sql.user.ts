4b0db2ca500cced64c66d1afa7e93c7ff042ee6f
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-05-13 11:35:00 +0200

    create get-user-route

--- news-app/apps/server/src/app/routes/sql/route.sql.user.ts
+++ news-app/apps/server/src/app/routes/sql/route.sql.user.ts
@@ -1,3 +1,13 @@
+import { User } from '@news-app/api-model';
 import { Router } from 'express';
 
 export const userRouterSql = Router();
+
+userRouterSql.get('/users', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ error: "Couldn't get users" });
+  }
+});
