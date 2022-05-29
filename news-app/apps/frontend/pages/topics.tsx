b431df802769d3f3fba092138b2f2303a6b1f847
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-05-29 19:16:22 +0200

    adding button component

--- news-app/apps/frontend/pages/topics.tsx
+++ news-app/apps/frontend/pages/topics.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { GetTopicResponse, getTopics } from '../modules/Api/topic/api.topic';
+import Button from '../modules/Button/Button';
 import { useFetch } from '../modules/common/hooks/common.hooks';
+import PageHeader from '../modules/Page/header/PageHeader';
 
 export default function TopicPage() {
   const { data, isLoading, error } = useFetch<GetTopicResponse>(getTopics);
 
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex justify-between">
+        <PageHeader title="Discussion Topics" />
+        <Button>Create New Topic</Button>
+      </div>
+    </div>
+  );
 }
