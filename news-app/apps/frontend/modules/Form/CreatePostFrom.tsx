e407816c984bd4f71ecc5420e2bb938b04ea6ec6
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-06-03 15:16:59 +0200

    addign create post form

---
+++ news-app/apps/frontend/modules/Form/CreatePostFrom.tsx
@@ -0,0 +1,47 @@
+import { IDiscussionPostPropsCreate } from '@news-app/api-model';
+import React from 'react';
+
+interface CreatePostFormProps {
+  inputs: IDiscussionPostPropsCreate;
+  setInputs: React.Dispatch<React.SetStateAction<IDiscussionPostPropsCreate>>;
+  onSubmit: () => Promise<void>;
+  submitButton: React.ReactNode;
+}
+
+export default function CreatePostForm({
+  inputs,
+  setInputs,
+  onSubmit,
+  submitButton,
+}: CreatePostFormProps) {
+  const handleOnChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
+    const { name, value } = e.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await onSubmit();
+      }}
+      className="space-y-8"
+    >
+      <textarea
+        value={inputs.text}
+        className="textarea textarea-primary w-full"
+        name="text"
+        placeholder="Pleans enter a text"
+        onChange={handleOnChange}
+      />
+      {submitButton}
+    </form>
+  );
+}
