0e4dd7dc8ea595f59583fd88892b6075a92b3b66
Fabian Frauenberger <fabian.frauenberger@me.com>
2022-06-03 11:27:25 +0200

    added new route to get post replies

--- news-app/apps/server/src/app/routes/sql/route.sql.post.ts
+++ news-app/apps/server/src/app/routes/sql/route.sql.post.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import {createPost,getPostsOfTheme} from '../../controllers/sql/controller.sql.post'
+import {createPost,getPostsOfTheme,getRepliesOfPost} from '../../controllers/sql/controller.sql.post'
 
 export const postRouterSql = Router();
 
 postRouterSql.get("/posts/:id",getPostsOfTheme);
+
+postRouterSql.get("/posts/:id/:postId",getRepliesOfPost);
 
 postRouterSql.post("/posts",createPost);
