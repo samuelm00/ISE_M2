003de768bd34793baafccc935dbcd2fb89af70ae
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-06-08 22:34:52 +0200

    adding db provider to root

--- news-app/apps/frontend/pages/_app.tsx
+++ news-app/apps/frontend/pages/_app.tsx
@@ -4,26 +4,29 @@ import Navbar from '../modules/Navigation/Navbar';
 import PageLayout from '../modules/Page/layout/PageLayout';
 import { RoutePath } from '../modules/Navigation/common/constants/constant.route';
 import AuthProvider from '../provider/Auth/AuthProvider';
+import DbProvider from '../provider/Db/DbProvider';
 
 function CustomApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
-      <AuthProvider>
-        {router.route !== RoutePath.Login ? (
-          <nav>
-            <Navbar />
-          </nav>
-        ) : null}
-        <main>
-          {router.route === RoutePath.Login ? (
-            <Component {...pageProps} />
-          ) : (
-            <PageLayout>
+      <DbProvider>
+        <AuthProvider>
+          {router.route !== RoutePath.Login ? (
+            <nav>
+              <Navbar />
+            </nav>
+          ) : null}
+          <main>
+            {router.route === RoutePath.Login ? (
               <Component {...pageProps} />
-            </PageLayout>
-          )}
-        </main>
-      </AuthProvider>
+            ) : (
+              <PageLayout>
+                <Component {...pageProps} />
+              </PageLayout>
+            )}
+          </main>
+        </AuthProvider>
+      </DbProvider>
     </>
   );
 }
