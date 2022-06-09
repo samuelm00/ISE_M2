938b55dd5fb12d6aa5441c4329a2e5713a287e32
Samuel Mitterrutzner <sa.mitterrutzner@gmail.com>
2022-06-09 12:29:14 +0200

    addind db variant switch

--- news-app/apps/frontend/pages/profile.tsx
+++ news-app/apps/frontend/pages/profile.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import Button from '../modules/Button/Button';
+import { useDbVariant } from '../provider/Db/hook.db-provider';
 
 export default function ProfilePage() {
-  return <div></div>;
+  const [dbVariant, setDbVariant] = useDbVariant();
+  console.log(dbVariant);
+
+  return (
+    <div>
+      <Button
+        className="btn-primary"
+        onClick={() =>
+          setDbVariant((prev) => (prev === 'sql' ? 'nosql' : 'sql'))
+        }
+      >
+        SwitchDB
+      </Button>
+    </div>
+  );
 }
