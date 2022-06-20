5fcb8529e39f134cb4fcc12ce92c4366a6f39682
Fabian Frauenberger <fabian.frauenberger@me.com>
2022-06-20 23:38:10 +0200

    user migration

--- news-app/apps/server/src/app/db-filler/nosql/db-filler.user.ts
+++ news-app/apps/server/src/app/db-filler/nosql/db-filler.user.ts
@@ -1,20 +1,16 @@
-import { IUserComplete, User } from '@news-app/api-model';
-import { createUser } from '../../controllers/nosql/controller.nosql.user';
+import { User, UserNoSql } from '@news-app/api-model';
 
-export async function addBaseUsers() {
+export const usersIdMap = new Map();
+
+export async function migrateBaseUsers() {
   const users = await User.findAll();
-  return Promise.all(users.map(async (user) => _createUser(user)));
+  return Promise.all(users.map(async (user) => {
+    const userNoSql = await UserNoSql.create({
+        email: user.email,
+        password: user.password
+      });
+      usersIdMap.set(user.id, userNoSql._id);
+      return user;
+  }));
 }
 
-/**
- *
- * @param nr
- * @returns
- */
-async function _createUser(testUser: IUserComplete) {
-  const user: Omit<IUserComplete, 'id'> = {
-    email: testUser.email,
-    password: testUser.password,
-  };
-  return createUser(user);
-}
