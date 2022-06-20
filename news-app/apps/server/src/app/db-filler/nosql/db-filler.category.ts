17cf59dfd55dafe0d7dc4c23f65ca7118a99c972
Fabian Frauenberger <fabian.frauenberger@me.com>
2022-06-20 23:38:22 +0200

    category migration

--- news-app/apps/server/src/app/db-filler/nosql/db-filler.category.ts
+++ news-app/apps/server/src/app/db-filler/nosql/db-filler.category.ts
@@ -1,14 +1,16 @@
 import { DiscussionCategory, DiscussionCategoryNoSql } from '@news-app/api-model';
 
+export const categoriesIdMap = new Map();
+
 export async function fillCategoryTable() {
   const categories = await DiscussionCategory.findAll();
-  return Promise.all(
-    categories.map(async (category) => {
-      return await DiscussionCategoryNoSql.create({
+    return Promise.all(categories.map(async (category) => {
+      const categoryNoSql =  await DiscussionCategoryNoSql.create({
         name:category.name,
         description: category.description,
         users: [],
       });
-    })
-  );
+      categoriesIdMap.set(category.id, categoryNoSql._id);
+      return category;
+    }));
 }
